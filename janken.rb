743ecd38616acc4b7a65b9998e5c2dc0e4dc9613
pingguo-ringo <keaitaromi@gmail.com>
2022-09-12 13:33:21 +0000

    fixed an error

--- janken.rb
+++ janken.rb
@@ -5,13 +5,13 @@ def janken_game
     my_hand = gets.to_i
     puts "ホイ！"
       
-    if my_hand == 3
-      puts "ゲームを終了します"
-      exit
-    elsif my_hand > 3
-      puts "正しい数値を入力してください"
-      janken_game
-    end
+      if my_hand == 3
+        puts "ゲームを終了します"
+        exit
+      elsif my_hand > 3
+        puts "正しい数値を入力してください"
+        janken_game
+      end
       
     com_hand = rand(3)
     jankens = ["グー","チョキ","パー"]
@@ -23,16 +23,16 @@ def janken_game
       
     $win = "win"
       
-    if my_hand == com_hand
-      puts "あいこで"
-      janken_game
-    elsif  (my_hand = 0 && com_hand = 1) || (my_hand = 1 && com_hand = 2) || (my_hand = 2 && com_hand = 0)
-      $win = "win"
-      attimuite_hoi
-    elsif  (my_hand = 0 && com_hand = 2) || (my_hand = 1 && com_hand = 0) || (my_hand = 2 && com_hand = 1)
-      $win = "lose"
-      attimuite_hoi
-    end
+      if my_hand == com_hand
+        puts "あいこで"
+        janken_game
+      elsif  (my_hand == 0 && com_hand == 1) || (my_hand == 1 && com_hand == 2) || (my_hand == 2 && com_hand == 0)
+        $win = "win"
+        attimuite_hoi
+      elsif  (my_hand == 0 && com_hand == 2) || (my_hand == 1 && com_hand == 0) || (my_hand == 2 && com_hand == 1)
+        $win = "lose"
+        attimuite_hoi
+      end
 end  
   
 def attimuite_hoi 
@@ -45,17 +45,17 @@ def attimuite_hoi
   com_direction = rand(4)
   directions=["上","下","左","右"]
       
-  puts "あなた：#{directions[my_direction]}を向きました"
-  puts "相手：#{directions[com_direction]}を向きました"
+  puts "あなた：#{directions[my_direction]}"
+  puts "相手：#{directions[com_direction]}"
   puts "----------------"
     
-  if (my_direction == com_direction) && ($win == "win")
-      puts "あなたの勝ちです"
-  elsif (my_direction == com_direction) && ($win == "lose")
-      puts "あなたの負けです"
-  else 
-      janken_game
-  end
+    if (my_direction == com_direction) && ($win == "win")
+        puts "あなたの勝ちです"
+    elsif (my_direction == com_direction) && ($win == "lose")
+        puts "あなたの負けです"
+    else 
+        janken_game
+    end
 end
   
   janken_game
